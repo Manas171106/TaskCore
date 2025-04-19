ee25b3ae8f1696fcea528e081be5a50c9b98269a
Mehar <mehar0454.becse24@chitkara.edu.in>
2025-04-19 23:37:12 +0530

    updated user route

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,9 +1,18 @@
 const express=require("express")
-const signin=require("../controllers/controller.js")
+const controller=require("../controllers/controller.js")
+const authenticate =require("../middelwares/auth.js")
 
 const router=express.Router()
 
-router.post("/register",signin)
+router.post("/register",controller.signin)
+
+router.post("/login",controller.login)
+
+router.post("/logout",controller.logout)
+
+router.get("/profile",authenticate,(req,res)=>{
+    console.log(req.user)
+})
 
 module.exports=router
 
