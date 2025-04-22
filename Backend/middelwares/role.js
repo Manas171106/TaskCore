80f3b795dcd2cf22e7445b716386a65e03662a83
mehar <mehar0454.becse24@chitkara.edu.in>
2025-04-22 15:15:23 +0530

    role middelware complete

--- Backend/middelwares/role.js
+++ Backend/middelwares/role.js
@@ -5,7 +5,14 @@ const allowRoles = (role) => {
       if (!userRole) {
         return res.status(401).json("Unauthorized: role not found in token");
       }
-
+    
+      if (!(role === userRole)) {
+        console.log(userRole)
+        return res.status(403).json("Access denied: insufficient permissions");
+      }
+  
+      console.log("Role check passed:", userRole);
+      next();
     };
   };
   
