8f972fb0b91ab8bd8f7c727a2241351cbc66985c
mehar <mehar0454.becse24@chitkara.edu.in>
2025-04-22 15:19:07 +0530

    started frontend again

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import "./index.css"
-
 import Signin from './pages/Signin'
 import Lognin from './pages/loginpage'
 import AdminDashboard from "./pages/AdminDashboard"
 import EmployeeDashboard from "./pages/EmployeeDashboard"
-
 
 function App() {
 
   return (
     <>
 
-    <Signin/>
+    {/* <Signin/> */}
+    <Lognin/>
+    {/* <AdminDashboard/>
+    <EmployeeDashboard/> */}
 
     </>
   )
