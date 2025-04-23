bd8aae154fe3834794d3445342058101e455ac0d
YourGitHubUsername <your_email@example.com>
2025-04-23 21:37:17 +0530

    Changinging in admin dashboard

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -1,24 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/adminpage.css"
 import Headers from '../components/Headers'
+import axios from "axios"
 
 const AdminDashboard = () => {
+
+  const [users,setusers]=useState([])
+  const [title,settitle]=useState("")
+  const [description,setdescription]=useState("")
+  const [date,setdate]=useState("")
+  const [assignTo,setassignTo]=useState("")
+  const [category,setcategory]=useState("")
+
+  const handletask=async (e)=>{
+    e.preventDefault()
+    let formdata = {title,description,date,assignTo,category}
+    try{
+      await axios.post("http://localhost:3000/user/create",formdata,{
+        withCredentials: true,
+      })
+      alert("Task created successfully!");
+
+      const res = await axios.get("http://localhost:3000/user/allusers", {
+        withCredentials: true,
+      });
+      setusers(res.data);
+    }catch(err){
+      console.log("task error: ",err)
+    }
+    settitle("")
+    setdescription("")
+    setdate("")
+    setcategory("")
+    setassignTo("")
+  }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/user/allusers", {
+          withCredentials: true,
+        });
+        setusers(res.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    })();
+  }, []);
+  
   return (
     <div className="admin">
       <Headers/>
         <div className="admin-hero">
         <div className="admin-left">
-            <form>
+            <form onSubmit={(e)=>{
+              handletask(e)
+            }}>
                 <label htmlFor="title">Title  </label>
-                <input type="text" name="title" placeholder='Enter Title' />
+                <input type="text" name="title" onChange={(e)=>{
+                  settitle(e.target.value)
+                }} placeholder='Enter Title' />
                 <label htmlFor="Date">Date</label>
-                <input type="Date" name="Date" placeholder='Enter Date' />
+                <input type="Date" name="Date" onChange={(e)=>{
+                  setdate(e.target.value)
+                }}  placeholder='Enter Date' />
                 <label htmlFor="Assign">Assign to</label>
-                <input type="text" name="Assign" placeholder='Assign to'/>
+                <input type="text" name="Assign" onChange={(e)=>{
+                  setassignTo(e.target.value)
+                }}  placeholder='Assign to'/>
                 <label htmlFor="Category">Category</label>
-                <input type="text" name="Category" placeholder='DEv,BUG,Etc..'/>
+                <input type="text" name="Category" onChange={(e)=>{
+                  setcategory(e.target.value)
+                }}  placeholder='DEv,BUG,Etc..'/>
                 <label htmlFor="Description">Description</label>
-                <textarea name='Description'></textarea>
+                <textarea onChange={(e)=>{
+                  setdescription(e.target.value)
+                }}  name='Description'></textarea>
                 <input type="submit" value="Create"/>
             </form>
         </div>
@@ -33,30 +90,35 @@ const AdminDashboard = () => {
               <h3>All Tasks</h3>
             </div>
             <div className="status">
-              <div className="employee-status">
-                <div className="name">
-                <h3>Alice jhonsan</h3></div>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-              </div>
-              <div className="employee-status">
-                <div className="name">
-                <h3>Alice jhonsan</h3></div>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-              </div>
-              <div className="employee-status">
-                <div className="name">
-                <h3>Alice jhonsan</h3></div>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-                <h3>0</h3>
-              </div>
+              {
+                users.map((user)=>{
+                  let completed=0;
+                  let failed=0
+                  let active=0;
+                  let total=user.tasks.length
+                  const tasks=user.tasks
+                  tasks.forEach(task => {
+                    if(task.status=="completed"){
+                      completed+=1;
+                    }else if(task.status=="failed"){
+                      failed+=1;
+                    }else{
+                      active+=1;
+                    }
+                  });
+                  return (
+                    <div key={user._id} className="employee-status">
+                      <div className="name">
+                        {user.fullname}
+                      </div>
+                        <h3>{active}</h3>
+                        <h3>{completed}</h3>
+                        <h3>{failed}</h3>
+                        <h3>{total}</h3>
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div></div>
