fade4e39d5d73bd177d6c7260b7094211210798f
vansh <vansh0483.becse24@chitkara.edu.in>
2025-04-24 10:02:38 +0530

    fixing errors in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
+    
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
