1340f01d2f101e5ffde5391083a2a874c4b7f08d
Mehar <mehar0454.becse24@chitkara.edu.in>
2025-04-24 09:43:06 +0530

    fixed conflicts in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,17 +9,10 @@ function App() {
   return (
     <>
 
-<<<<<<< HEAD
     <Signin/>
     <Login/>
     <AdminDashboard/>
     <EmployeeDashboard/>
-=======
-    {/* <Signin/> */}
-    <Lognin/>
-    {/* <AdminDashboard/>
-    <EmployeeDashboard/> */}
->>>>>>> 62628c99b9bea1365ad0aa3f0740e05f505ae5d4
 
     </>
   )
